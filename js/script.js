6e2b97bd51d3ba5c6a18c43650cd0a79f86087c0
CimarSantos <francimarlsantos@outlook.com>
2022-05-02 17:35:02 -0300

    Pesquisa simples de todos os tópicos

--- js/script.js
+++ js/script.js
@@ -2,7 +2,30 @@ showThree = () => {
     document.querySelector(".three").classList.toggle("esconde");
 }
 
-topics = () => {
-    document.querySelector(".corpo").classList.toggle("esconde");
+topic1 = () => {
+    document.querySelector(".topico1-DOM").classList.toggle("esconde");
+}
 
+topic2 = () => {
+    document.querySelector(".topico2-winDocument").classList.toggle("esconde");
+}
+
+topic3 = () => {
+    document.querySelector(".topico3-diffGetQuery").classList.toggle("esconde");
+}
+
+topic4 = () => {
+    document.querySelector(".topico4-elementJavascript").classList.toggle("esconde");
+}
+
+topic5 = () => {
+    document.querySelector(".topico5-diffInnerTextContent").classList.toggle("esconde");
+}
+
+topic6 = () => {
+    document.querySelector(".topico6-diffNodeListArray").classList.toggle("esconde");
+}
+
+topic7 = () => {
+    document.querySelector(".topico7-criarElementos").classList.toggle("esconde");
 }
